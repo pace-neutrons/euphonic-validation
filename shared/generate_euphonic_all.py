07e0d64b83b85dd9e0cdd8d8212ed39cff9a7165
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-05-13 15:16:24 +0000

    Add CASTEP LZO/Quartz files

--- shared/generate_euphonic_all.py
+++ shared/generate_euphonic_all.py
@@ -1,8 +1,8 @@
 from generate_euphonic_dw import main as dw_main
 from generate_euphonic_sf import main as sf_main
 
-dw_fc = dw_main(['../quartz', '--grid', '4,4,4', '--temp', '5'])
-dw_phonons = dw_main(['../quartz', '--grid', '4,4,4', '--temp', '5', '--freqs'])
+dw_fc = dw_main(['../quartz', '--grid', '6,6,6', '--temp', '5'])
+dw_phonons = dw_main(['../quartz', '--grid', '6,6,6', '--temp', '5', '--freqs'])
 
 sf_main(['../quartz/2ph_m4_0_ECut/', '--dw', dw_fc])
 sf_main(['../quartz/2ph_m4_0_ECut/', '--dw', dw_phonons, '--freqs'])
