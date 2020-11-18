a12491f5234d278883a77483d9b80593eac61f77
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-11-18 11:32:59 +0000

    Update publication plots
    Ensure correct axes - from negative to positive

--- plot_publication_figures.py
+++ plot_publication_figures.py
@@ -23,28 +23,31 @@ def get_ebins(material, bin_width=0.01):
     elif material == 'nb':
         return np.arange(0, 30, bin_width)*ureg('meV')
 
-def get_fig(material, cut, x_data_idx=None, **plot_kwargs):
+def get_fig(material, cut, x_data_idx=None, negative_idx=False,
+            **plot_kwargs):
     sf = get_sf(material, cut)
     sqw = sf.calculate_sqw_map(get_ebins(material))
-    sqw_orig = sqw
     sqw = sqw.broaden(y_width=1.5*ureg('meV'))
     if x_data_idx != None:
         sqw.x_tick_labels = None
-        sqw._x_data = sf.qpts[:, x_data_idx]
+        sqw._x_data = sf.qpts[:, np.absolute(x_data_idx)]
+        if negative_idx:
+            sqw._x_data = np.negative(sqw._x_data)
+
     fig = plot_2d(sqw, y_label='Energy (meV)', **plot_kwargs)
     return fig
 
-fig1 = get_fig('quartz', '30L_qe_fine', 2, ratio=0.6, vmax=1e-11,
-               x_label='[3,0,L]')
+fig1 = get_fig('quartz', '30L_qe_fine', 2, negative_idx=True,
+               vmax=1e-11, x_label='[-3,0,-L]')
 fig2 = get_fig('quartz', '2ph_m4_0_qe', 0, vmax=1e-11,
                x_label='[H,-4,0]')
-fig3 = get_fig('lzo', 'kagome_qe', 2, ratio=0.6, vmax=1e-9,
-               x_label='[5,7,L]')
-fig4 = get_fig('lzo', 'hh2_qe_fine', 0, ratio=0.6, vmax=1e-11,
-               x_label='[H,H,-2]')
-fig5 = get_fig('nb', '110_qe', 0, ratio=0.6, vmax=1e-11,
+fig3 = get_fig('lzo', 'kagome_qe', -2, negative_idx=True, vmax=1e-11,
+               x_label='[-5,7,-L]')
+fig4 = get_fig('lzo', 'hh2_qe_fine', 0, vmax=1e-11,
+               x_label='[H,-H,-2]')
+fig5 = get_fig('nb', '110_qe', 0, vmax=1e-11,
                x_label='[H,H,0]')
-fig6 = get_fig('nb', 'm110_qe', 0, ratio=0.6, vmax=1e-11,
-               x_label='[H,-H,0]')
+fig6 = get_fig('nb', 'm110_qe', 1, vmax=1e-11,
+               x_label='[2-K,K,0]')
 
 matplotlib.pyplot.show()
