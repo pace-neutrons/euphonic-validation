bd299324816709cbc8314a5760dbdde3e1436c33
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-12-02 09:05:36 +0000

    Update to use Debye-Waller from Euphonic freqs

--- shared/generate_euphonic_data/run_generate_euphonic.py
+++ shared/generate_euphonic_data/run_generate_euphonic.py
@@ -16,13 +16,7 @@ quartz_cuts = ['2ph_m4_0_qe', '30L_qe_fine']
 for temp in quartz_temps:
     for cut in quartz_cuts:
         sf_main(['../../quartz/' + cut + '/', '--dw', '../../quartz/shared/euphonic/dw_fc_666_' + temp + 'K.json'])
-        sf_main(['../../quartz/' + cut + '/', '--dw', '../../quartz/shared/euphonic/dw_phonons_666_' + temp + 'K.json', '--freqs'])
-
-# Generate Ab2tds S(Q,w) map for Quartz
-#for cut in quartz_cuts:
-#    sqw_main(['../../quartz/' + cut + '/euphonic/sf_fc_100K.json', '--ab2tds',
-#              '--ofig', '../../quartz/' + cut + '/ab2tds/euphonic_100K.pdf',
-#              '--osqw', '../../quartz/' + cut + '/ab2tds/sqw_euphonic_100K.json'])
+        sf_main(['../../quartz/' + cut + '/', '--dw', '../../quartz/shared/euphonic/dw_fc_666_' + temp + 'K.json', '--freqs'])
 
 # Generate OClimax S(Q,w) map for Quartz
 for cut in quartz_cuts:
@@ -46,14 +40,7 @@ lzo_cuts = ['kagome_qe', 'hh2_qe_fine']
 for temp in lzo_temps:
     for cut in lzo_cuts:
         sf_main(['../../lzo/' + cut + '/', '--dw', '../../lzo/shared/euphonic/dw_fc_666_' + temp + 'K.json'])
-        sf_main(['../../lzo/' + cut + '/', '--dw', '../../lzo/shared/euphonic/dw_phonons_666_' + temp + 'K.json', '--freqs'])
-
-# Generate Ab2tds S(Q,w) map for LZO
-#for cut in lzo_cuts:
-#    for temp in lzo_temps:
-#        sqw_main(['../../lzo/' + cut + '/euphonic/sf_fc_' + temp + 'K.json', '--ab2tds',
-#                  '--ofig', '../../lzo/' + cut + '/ab2tds/euphonic_' + temp + 'K.pdf',
-#                  '--osqw', '../../lzo/' + cut + '/ab2tds/sqw_euphonic_' + temp + 'K.json'])
+        sf_main(['../../lzo/' + cut + '/', '--dw', '../../lzo/shared/euphonic/dw_fc_666_' + temp + 'K.json', '--freqs'])
 
 # Generate OClimax S(Q,w) map for LZO
 for cut in lzo_cuts:
@@ -77,14 +64,7 @@ nb_cuts = ['110_qe', 'm110_qe']
 for temp in nb_temps:
     for cut in nb_cuts:
         sf_main(['../../nb/' + cut + '/', '--dw', '../../nb/shared/euphonic/dw_fc_666_' + temp + 'K.json'])
-        sf_main(['../../nb/' + cut + '/', '--dw', '../../nb/shared/euphonic/dw_phonons_666_' + temp + 'K.json', '--freqs'])
-
-# Generate Ab2tds S(Q,w) map for Nb
-#for cut in nb_cuts:
-#    for temp in nb_temps:
-#        sqw_main(['../../nb/' + cut + '/euphonic/sf_fc_' + temp + 'K.json', '--ab2tds',
-#                  '--ofig', '../../nb/' + cut + '/ab2tds/euphonic_' + temp + 'K.pdf',
-#                  '--osqw', '../../nb/' + cut + '/ab2tds/sqw_euphonic_' + temp + 'K.json'])
+        sf_main(['../../nb/' + cut + '/', '--dw', '../../nb/shared/euphonic/dw_fc_666_' + temp + 'K.json', '--freqs'])
 
 # Generate OClimax S(Q,w) map for Nb
 for cut in nb_cuts:
