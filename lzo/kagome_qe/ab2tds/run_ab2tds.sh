3c7257064aaca90b518fae50727ad14c636c4719
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-04-29 14:10:29 +0000

    Run Ab2tds cuts with new grids

--- lzo/kagome_qe/ab2tds/run_ab2tds.sh
+++ lzo/kagome_qe/ab2tds/run_ab2tds.sh
@@ -1,4 +1,4 @@
 make_TDS_DispersionIntensityCurvesClone \
    ../castep/La2Zr2O7.phonon \
    intensity_map_input.py \
-   ../../shared/ab2tds/La2Zr2O7-666-grid.phonon.md5_code=801b2c549e4ec5a50059b1eb2a5a3778
+   ../../shared/ab2tds/La2Zr2O7-444-full-grid.phonon.md5_code=c20191af30f9ef722fe109b0d7d0c0be
