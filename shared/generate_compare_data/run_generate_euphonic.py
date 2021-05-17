38eca80452f9a5b56861e8b339856baf7598af99
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-05-17 14:16:22 +0100

    Simplify Euphonic generation script

--- shared/generate_compare_data/run_generate_euphonic.py
+++ shared/generate_compare_data/run_generate_euphonic.py
@@ -5,65 +5,28 @@ from generate_euphonic_sf import main as sf_main
 from generate_euphonic_sqw import main as sqw_main
 from util import get_material_info, get_dir, get_euphonic_fname, get_euphonic_fpath
 
+materials = ['quartz', 'lzo', 'nb']
+for mat in materials:
+    cuts, grid, temps = get_material_info(mat)
 
-# Generate Debye-Waller for different temperatures for Quartz
-quartz_cuts, quartz_grid, quartz_temps = get_material_info('quartz')
-for temp in quartz_temps:
-    dw_fc = dw_main(['quartz', '--grid', quartz_grid, '--temp', temp])
-    dw_phonons = dw_main(['quartz', '--grid', quartz_grid, '--temp', temp, '--freqs'])
-# Generate structure factors for different temperatures for Quartz
-for temp in quartz_temps:
-    for cut in quartz_cuts:
-        sf_main(['quartz', cut,
-                 '--dw', get_euphonic_fpath('quartz', 'euphonic', 'dw', temp, from_fc=True, grid=quartz_grid)])
-        sf_main(['quartz', cut,
-                 '--dw', get_euphonic_fpath('quartz', 'euphonic', 'dw', temp, from_fc=False, grid=quartz_grid),
-                 '--freqs'])
-# Generate OClimax S(Q,w) map for Quartz
-for cut in quartz_cuts:
-    for temp in quartz_temps:
-        sqw_main(['quartz', cut, '--temp', temp,
-                  '--ofig', os.path.join(get_dir('quartz', cut=cut, code='euphonic'),
-                                         'euphonic_sqw_'+ temp + 'K.pdf')])
-        sqw_main(['quartz', cut, '--temp', temp, '--freqs'])
-# Generate Debye-Waller for LZO
-lzo_cuts, lzo_grid, lzo_temps = get_material_info('lzo')
-for temp in lzo_temps:
-    dw_fc = dw_main(['lzo', '--grid', lzo_grid, '--temp', temp])
-    dw_phonons = dw_main(['lzo', '--grid', lzo_grid, '--temp', temp, '--freqs'])
-# Generate structure factors for LZO
-for temp in lzo_temps:
-    for cut in lzo_cuts:
-        sf_main(['lzo', cut,
-                 '--dw', get_euphonic_fpath('lzo', 'euphonic', 'dw', temp, from_fc=True, grid=lzo_grid)])
-        sf_main(['lzo', cut,
-                 '--dw', get_euphonic_fpath('lzo', 'euphonic', 'dw', temp, from_fc=False, grid=lzo_grid),
-                 '--freqs'])
-# Generate OClimax S(Q,w) map for LZO
-for cut in lzo_cuts:
-    for temp in lzo_temps:
-        sqw_main(['lzo', cut, '--temp', temp,
-                  '--ofig', os.path.join(get_dir('lzo', cut=cut, code='euphonic'),
-                                         'euphonic_sqw_'+ temp + 'K.pdf')])
-        sqw_main(['lzo', cut, '--temp', temp, '--freqs'])
+    # Generate Debye-Waller for different temperatures
+    for temp in temps:
+        dw_fc = dw_main([mat, '--grid', grid, '--temp', temp])
+        dw_phonons = dw_main([mat, '--grid', grid, '--temp', temp, '--freqs'])
 
-# Generate Debye-Waller for Nb
-nb_cuts, nb_grid, nb_temps = get_material_info('nb')
-for temp in nb_temps:
-    dw_fc = dw_main(['nb', '--grid', nb_grid, '--temp', temp])
-    dw_phonons = dw_main(['nb', '--grid', nb_grid, '--temp', temp, '--freqs'])
-# Generate structure factors for Nb
-for temp in nb_temps:
-    for cut in nb_cuts:
-        sf_main(['nb', cut,
-                 '--dw', get_euphonic_fpath('nb', 'euphonic', 'dw', temp, from_fc=True, grid=nb_grid)])
-        sf_main(['nb', cut,
-                 '--dw', get_euphonic_fpath('nb', 'euphonic', 'dw', temp, from_fc=False, grid=nb_grid),
-                 '--freqs'])
-# Generate OClimax S(Q,w) map for Nb
-for cut in nb_cuts:
-    for temp in nb_temps:
-        sqw_main(['nb', cut, '--temp', temp,
-                  '--ofig', os.path.join(get_dir('nb', cut=cut, code='euphonic'),
-                                         'euphonic_sqw_'+ temp + 'K.pdf')])
-        sqw_main(['nb', cut, '--temp', temp, '--freqs'])
+    # Generate structure factors for different temperatures
+    for temp in temps:
+        for cut in cuts:
+            sf_main([mat, cut,
+                     '--dw', get_euphonic_fpath(mat, 'euphonic', 'dw', temp, from_fc=True, grid=grid)])
+            sf_main([mat, cut,
+                     '--dw', get_euphonic_fpath(mat, 'euphonic', 'dw', temp, from_fc=False, grid=grid),
+                     '--freqs'])
+
+    # Generate S(Q,w) map
+    for temp in temps:
+        for cut in cuts:
+            sqw_main([mat, cut, '--temp', temp,
+                      '--ofig', os.path.join(get_dir(mat, cut=cut, code='euphonic'),
+                                             'euphonic_sqw_'+ temp + 'K.pdf')])
+            sqw_main([mat, cut, '--temp', temp, '--freqs'])
