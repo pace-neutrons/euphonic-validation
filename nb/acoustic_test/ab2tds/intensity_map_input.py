5e241bb99d65bb5829d7e297b73303dcfd077d46
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-05-23 13:02:59 +0100

    Add near-gamma acoustic q-points to test units

---
+++ nb/acoustic_test/ab2tds/intensity_map_input.py
@@ -0,0 +1,17 @@
+import numpy as np
+APPLYTIMEREVERSAL=0
+Temperature=300
+resolutionfile='resolution.txt'
+Saturation=0.2
+lowerLimit=0
+bottom_meV=0
+NEUTRONCALC=1
+CohB={'Nb': 7.054}
+NeutronE=1e10
+branchWeight=np.ones(3).tolist()
+EigScal=0
+UniqueIon=-1
+
+redStarts = [[0.8, 0.8, 0.0]]
+redEnds = [[1.2, 1.2, 0.0]]
+Nqlines = [41]
