7bc68d8fa57c2dfdb688e76cedd8142fc0bb7a49
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-05-26 10:35:07 +0000

    Update oclimax outputs

--- nb/m110_qe/oclimax/run_oclimax.sh
+++ nb/m110_qe/oclimax/run_oclimax.sh
@@ -1,6 +1,6 @@
-cp ../castep/Nb-181818-s0.5-NCP19-vib-disp.phonon .
-sudo oclimax convert -c Nb-181818-s0.5-NCP19-vib-disp.phonon -o nb
-rm Nb-181818-s0.5-NCP19-vib-disp.phonon
+#cp ../castep/Nb-181818-s0.5-NCP19-vib-disp.phonon .
+#sudo oclimax convert -c Nb-181818-s0.5-NCP19-vib-disp.phonon -o nb
+#rm Nb-181818-s0.5-NCP19-vib-disp.phonon
 cp ../../shared/oclimax/Nb-181818-s0.5-NCP19-vib-disp-101010-full-grid.oclimax .
 cp nb.params.copy nb.params
 sudo oclimax run Nb-181818-s0.5-NCP19-vib-disp-101010-full-grid.oclimax nb.params nb.oclimax
